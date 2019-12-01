f8b38bb40ad1ec1d0f697027de12a3f2a743a46a
Aljoscha Schmidt <Ceron257@users.noreply.github.com>
2019-12-01 15:34:19 +0100

    -simplified calculateFuelNeed -Removed debug output

--- day1/main.cpp
+++ day1/main.cpp
@@ -7,11 +7,11 @@ using namespace std;
 int calculateFuelNeed (int mass, bool recursive = true)
 {
   auto requiredFuel = (mass / 3) - 2;
-  if (recursive && requiredFuel > 0)
+  if (recursive && requiredFuel > 8)
   {
     return requiredFuel + calculateFuelNeed (requiredFuel);
   }
-  return requiredFuel < 0 ? 0 : requiredFuel;
+  return requiredFuel;
 }
 
 int main ()
@@ -19,8 +19,6 @@ int main ()
   const char* inputFile = INPUTS_PATH "/day1.txt";
 
   ifstream input(inputFile);
-
-  cout << "100756 -> " << calculateFuelNeed (100756) << endl;
 
   int neededFuel = 0;
   int neededFuelRecursive = 0;
